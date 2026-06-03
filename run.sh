e96adcc42fa851318deeed3179e2962f2725c6e0
root <root@hostname-paxgo.foreman.pxe>
2026-06-03 14:04:05 +0000

    run modified

--- run.sh
+++ run.sh
@@ -4,20 +4,23 @@ make
 #Node selection
 OBJ=CG_Umpire 
 #MATRIX
-export ROW_PTR=31
-export COL_IDX=31
-export VALUES=31
+export ROW_PTR=0
+export COL_IDX=0
+export VALUES=0
 
-export B=5
-export X=6
+export B=0
+export X=0
 
 #VECTORS
-export R=5
-export Z=6
-export P=7
-export AP=8
+export R=0
+export Z=0
+export P=0
+export AP=0
 
 #CHECK
-export AX=9
+export AX=0
 
+./$OBJ matrix.csr
+./$OBJ audikw_1.mtx 
 ./$OBJ /root/matrix/audikw_1/audikw_1.mtx 
+#./$OBJ matrix.csr
